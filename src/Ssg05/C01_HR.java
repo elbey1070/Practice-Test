b6719d5a9386c136a5f7c4a54ed1fa9469d21892
Elbay Shukur <elbey2433@gmail.com>
2023-09-28 23:08:57 +0400

    P Commit

---
+++ src/Ssg05/C01_HR.java
@@ -0,0 +1,21 @@
+package Ssg05;
+
+public class C01_IK {
+
+    public static int counter=100;
+
+    public String phone;
+    public String adress;
+    public String name;
+    public String surName;
+    public int id;
+
+    public int idAtama(){
+
+        id=counter;
+        counter++;
+        return id;
+    }
+
+
+    }
