12d37daee7396e47c987c2c8f2dabed5046f9b08
Elbay Shukur <elbey2433@gmail.com>
2023-10-10 20:33:51 +0400

    L C2

--- src/Ssg06/C01_exception.java
+++ src/Ssg06/C01_exception.java
@@ -1,8 +1,21 @@
 package Ssg06;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class C01_exception {
     public static void main(String[] args) {
 
-        int a= 5;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int age =0;
+
+        try {
+            age = scan.nextInt();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid data entered");
+        }
+        System.out.println(age);
     }
 }
